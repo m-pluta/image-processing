4006820de5b379b94f1110b666d6fcca930431fe
Michal Pluta <vsdc48@durham.ac.uk>
2024-02-12 17:40:03 +0000

    Simplify the classifier

--- measure.py
+++ measure.py
@@ -1,52 +1,35 @@
+import timeit
 import os
 import cv2
+
+SCALING = 1.0 / 255
+IMG_SIZE = (256, 256)
+MEAN = (0, 0, 0)
 
 
 def measure():
     model = cv2.dnn.readNetFromONNX('image_processing_files/classifier.model')
 
-    names = []
-    healthys = []
-    pneumonias = []
+    image_names = os.listdir('Results/')
 
-    # the first 50 images are healthy and the next 50 are not:
-    for i in range(1, 51):
-        healthys.append(f'im{str(i).zfill(3)}')
-
-    for i in range(51, 101):
-        pneumonias.append(f'im{str(i).zfill(3)}')
-
-    # read all the images from the directory
-
-    for file in os.listdir('Results/'):
-        names.append(file)
-    names.sort()
-
-    # keeping track of the number of correct predictions for accuracy:
     correct = 0
+    for image_name in image_names:
+        img = cv2.imread(os.path.join('Results/', image_name))
 
-    # main loop:
-    for filename in names:
+        blob = cv2.dnn.blobFromImage(
+            img, SCALING, IMG_SIZE, MEAN, swapRB=True, crop=False)
+        model.setInput(blob)
+        output = model.forward()
 
-        # read image:
-        img = cv2.imread(os.path.join('Results/', filename))
+        if (output > 0.5):
+            if (image_name[6] == 'p'):
+                correct += 1
+        else:
+            if (image_name[6] == 'h'):
+                correct += 1
 
-        if img is not None:
+    return correct / len(image_names)
 
-            # pass the image through the neural network:
-            blob = cv2.dnn.blobFromImage(
-                img, 1.0 / 255, (256, 256), (0, 0, 0), swapRB=True, crop=False)
-            model.setInput(blob)
-            output = model.forward()
 
-            # identify what the predicted label is:
-            if (output > 0.5):
-                # print(f'{filename}: pneumonia')
-                if (filename.startswith(tuple(pneumonias))):
-                    correct += 1
-            else:
-                # print(f'{filename}: healthy')
-                if (filename.startswith(tuple(healthys))):
-                    correct += 1
-
-    return correct / len(names)
+if __name__ == '__main__':
+    measure()
